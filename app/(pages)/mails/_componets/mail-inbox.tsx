6740b9da18a72ad63dffe2baa431df430ab14d6e
jatinjeet singh <jatinjeet@xcrino.com>
2024-12-17 16:22:26 +0530

    add badshaha code

--- app/(pages)/mails/_componets/mail-inbox.tsx
+++ app/(pages)/mails/_componets/mail-inbox.tsx
@@ -16,28 +16,36 @@ const MailInbox = () => {
   const dataIinbox = ["All", "Unread", "Unanswered"];
   return (
     <div>
-      <div className=" grid lg:grid-cols-2 gap-12 items-center justify-between bg-[#fafafa] text-[#525252] p-6 ">
-        <div className="grid grid-cols-4  items-center  w-full justify-between">
-          <label className="flex gap-4">
-            <input type="checkbox" aria-label="Select All" />
-            <span className=" font-medium ">Select All</span>
-          </label>
-          {dataIinbox.map((item, index) => ( 
-            <div key={index} className="">
-              <Button
-                variant="dark"
-                size="secondary"
-                className={`border rounded hover:bg-black hover:text-white ${
-                  activeDataInbox === index ? "bg-black text-white" : ""
-                }`}
-                onClick={() => setInboxData(index)}
-              >
-                {item}
-              </Button>
-            </div>
-          ))}
+      <div className="flex lg:flex-row flex-col justify-between gap-4 lg:gap-10 bg-[#fafafa] text-[#525252] p-6 ">
+        <div className="flex gap-2 gap-y-4 flex-wrap sm:flex-nowrap items-center   justify-between">
+          <div className="flex flex-nowrap items-center gap-2 ">
+            <input
+              type="checkbox"
+              aria-label="Select All"
+              className="h-5 w-5 rounded border-gray-300 focus:ring-2 focus:ring-[#f77705] text-white checked:bg-[#f77705] checked:border-[#f77705]"
+            />
+            <span className="font-medium text-nowrap text-gray-700">
+              Select All
+            </span>
+          </div>
+          <div className="flex gap-2">
+            {dataIinbox.map((item, index) => (
+              <div key={index} className="">
+                <Button
+                  variant="dark"
+                  size="secondary"
+                  className={`border rounded hover:bg-black hover:text-white ${
+                    activeDataInbox === index ? "bg-black text-white" : ""
+                  }`}
+                  onClick={() => setInboxData(index)}
+                >
+                  {item}
+                </Button>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className=" grid grid-cols-2  justify-items-end  ">
+        <div className="flex justify-between gap-2">
           <Button
             variant="dark"
             size="secondary"
@@ -46,7 +54,7 @@ const MailInbox = () => {
             Delete selected
           </Button>
           <Select>
-            <SelectTrigger className="lg:w-[180px]  border border-r-2 rounded font-medium hover:bg-black hover:text-white ">
+            <SelectTrigger className="lg:w-[180px] border border-r-2 rounded font-medium hover:bg-black hover:text-white">
               <SelectValue placeholder="More actions" />
             </SelectTrigger>
             <SelectContent>
@@ -63,9 +71,9 @@ const MailInbox = () => {
               <FcSearch className="text-7xl" />
             </div>
             <h2 className="mt-4 py-25 text-xl  font-bold">
-              You have no Letters in your Inbox 
+              You have no Letters in your Inbox
             </h2>
-            <p className="text-gray-500"> {`Let's change that!`}</p>
+            <p className="text-gray-500">{`Let's change that!`}</p>
             <div className="mt-4 flex items-center gap-2 px-6 py-2 bg-orange-500 text-white rounded hover:bg-orange-600 cursor-pointer">
               <p>Go to search</p>
               <LuUserSearch />
@@ -80,7 +88,7 @@ const MailInbox = () => {
               <FcSearch className="text-7xl" />
             </div>
             <h2 className="mt-4 py-25 text-xl  font-bold">
-            You have no Letters in your Inbox 
+              You have no Letters in your Inbox
             </h2>
             <p className="text-gray-500">{`Let's change that!`}</p>
             <div className="mt-4 flex items-center gap-2 px-6 py-2 bg-orange-500 text-white rounded hover:bg-orange-600 cursor-pointer">
@@ -97,7 +105,7 @@ const MailInbox = () => {
               <FcSearch className="text-7xl" />
             </div>
             <h2 className="mt-4 py-25 text-xl  font-bold">
-            You have no Letters in your Inbox 
+              You have no Letters in your Inbox
             </h2>
             <p className="text-gray-500">{`Let's change that!`}</p>
             <div className="mt-4 flex items-center gap-2 px-6 py-2 bg-orange-500 text-white rounded hover:bg-orange-600 cursor-pointer">
@@ -111,4 +119,3 @@ const MailInbox = () => {
   );
 };
 export default MailInbox;
-

f695dbc79824b69346df77d06476a5c603256167
jatinjeet singh <jatinjeet@xcrino.com>
2024-12-17 16:07:26 +0530

    fixed it

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className=""
+        className=" bg-smoke-mmm"
       >
         <Navbar/>
         {children}
